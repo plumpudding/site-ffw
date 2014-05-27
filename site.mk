fc40b3d13eabca406e54ce1440cf732602b937fc
Philipp Psurek <philipp.psurek@gmail.com>
2014-05-27 23:45:38 +0200

    5th tryout: site config converted to Lua
    see: https://github.com/freifunk-gluon/site-ffhl/commit/43ec5c7a6d630be3514483a5db46b726fa9b92ad

--- site.mk
+++ site.mk
@@ -16,3 +16,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
+DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)

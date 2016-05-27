e1020bd89794735ff5fb5ccd17437abc631aa10a
Milan Pässler <me@petabyteboy.de>
2016-05-28 01:01:08 +0200

    initial commit

---
+++ site.mk
@@ -0,0 +1,47 @@
+GLUON_SITE_PACKAGES := \
+  gluon-mesh-batman-adv-15 \
+  gluon-alfred \
+  gluon-respondd \
+  gluon-autoupdater \
+  gluon-config-mode-autoupdater \
+  gluon-config-mode-contact-info \
+  gluon-config-mode-core \
+  gluon-config-mode-geo-location \
+  gluon-config-mode-hostname \
+  gluon-config-mode-mesh-vpn \
+  gluon-ebtables-filter-multicast \
+  gluon-ebtables-filter-ra-dhcp \
+  gluon-luci-admin \
+  gluon-luci-autoupdater \
+  gluon-luci-portconfig \
+  gluon-luci-wifi-config \
+  gluon-next-node \
+  gluon-mesh-vpn-fastd \
+  gluon-radvd \
+  gluon-setup-mode \
+  gluon-status-page \
+  haveged \
+  iptables \
+  iwinfo \
+  iperf \
+
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+  kmod-usb-core \
+  kmod-usb2 \
+  kmod-usb-hid \
+  kmod-usb-net \
+  kmod-usb-net-asix \
+  kmod-usb-net-dm9601-ether \
+  kmod-r8169 \
+  vnstat
+endif
+
+DEFAULT_GLUON_RELEASE := wup-20160528
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en fr
+
+GLUON_TARGET ?= ar71xx-generic
+GLUON_BRANCH ?= stable
+
